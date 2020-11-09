dc3f51f90677bf0111ccc6a13bc138b86c30c396
K-O-K-C <74196400+K-O-K-C@users.noreply.github.com>
2020-11-09 20:38:56 +0400

    1456486

---
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -0,0 +1,53 @@
+package com.hfad.stopwatch;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import java.util.Locale;
+import android.os.Handler;
+import android.widget.TextView;
+public class StopwatchActivity extends Activity {
+    //Количество секунд на секундомере.
+    private int seconds = 0;
+    //Секундомер работает?
+    private boolean running;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runTimer();
+    }
+
+    //Запустить секундомер при щелчке на кнопке Start.
+    public void onClickStart(View view) {
+        running = true;
+    }
+    //Остановить секундомер при щелчке на кнопке Stop.
+    public void onClickStop(View view) {
+        running = false;
+    }
+    //Сбросить секундомер при щелчке на кнопке Reset.
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+    //Обновление показаний таймера.
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
